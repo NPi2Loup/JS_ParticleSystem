3892f3a5e79a5e09184cbf6c80cee884edca1dc0
Piedeloup Nicolas <npi2loup@gmail.com>
2024-03-29 14:43:38 +0100

    Update render_vs.glsl : use sqrt(mass)

--- n_body/render/webgl/shaders/render_vs.glsl
+++ n_body/render/webgl/shaders/render_vs.glsl
@@ -19,8 +19,9 @@ void main() {
     gl_Position = vec4(translated_pos * vec2(1, -1.0), 0, 1);
 
     gl_PointSize = point_size;
-    float coef = abs(mass/max_mass);
-    gl_PointSize += 5.0*coef;    
+    //float coef = abs(mass/max_mass);
+    //gl_PointSize += 5.0*coef;  
+    gl_PointSize *= sqrt(mass);
     gl_PointSize *= particle_scale;
 
     vec2 translated_velocity = 0.5 + velocity / max_speed * 0.5;
